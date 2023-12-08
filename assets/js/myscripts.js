a85511c85578f98e2136f4bcd6383a828b98a866
Daschwas <schwass.dale@gmail.com>
2023-12-08 19:10:20 +0800

    fix: phase 4 corrections

--- assets/js/myscripts.js
+++ assets/js/myscripts.js
@@ -1,9 +1,9 @@
-function getQuotes() {
+function getJokes() {
     try {
-        let selectedValue = document.querySelector('input[name="numberOfQuotes"]:checked').value;
+        let selectedValue = document.querySelector('input[name="numberOfJokes"]:checked').value;
         console.log('Selected value:', selectedValue);
 
-        let apiUrl = 'https://api.api-ninjas.com/v1/quotes?category=computers&limit=' + selectedValue;
+        let apiUrl = 'https://api.api-ninjas.com/v1/jokes?category=computers&limit=' + selectedValue;
         const apiKey = 'al/zHrzPIddGWNtdwcS+Kg==J6DqkicNGsAU45le'
 
         fetch(apiUrl, {
@@ -20,15 +20,15 @@ function getQuotes() {
                 console.log(data);
 
                 if (selectedValue !== 1 && data.length === 1) {
-                    console.log('Only one quote retrieved');
+                    console.log('Only one joke retrieved');
                 }
 
-                let quoteText = data.map(function(quoteData) {
-                    return `"${quoteData.quote}" - ${quoteData.author}`;
+                let jokeText = data.map(function(jokeData) {
+                    return `${jokeData.joke}`;
                 }).join('<br>'+'<br>');
 
-                let quotePlaceholder = document.getElementById('quotePlaceholder');
-                quotePlaceholder.innerHTML = quoteText;
+                let jokePlaceholder = document.getElementById('jokePlaceholder');
+                jokePlaceholder.innerHTML = jokeText;
             })
 
             .catch(function(error) {
@@ -36,8 +36,8 @@ function getQuotes() {
             });
 
     } catch (error) {
-        console.error('An error occurred in getting quotes:', error);
+        console.error('An error occurred in getting jokes:', error);
     }
 }
 
-document.getElementById('getQuotesButton').addEventListener('click', getQuotes);
+document.getElementById('getJokesButton').addEventListener('click', getJokes);
